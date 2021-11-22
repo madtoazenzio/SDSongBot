e1c106e8c43b050a13042d35075133c943b4acf6
Mad To AzenZio <89691212+madtoazenzio@users.noreply.github.com>
2021-11-22 19:50:33 +0530

    Update __main__.py

--- SDSongBot/__main__.py
+++ SDSongBot/__main__.py
@@ -13,7 +13,7 @@ pm_start_text = """
      
 Syntax : ```/dsong Faded```
       
-Powerd By @SDBotsz 🔥
+Powered By @Z_Bots 🔥
 """
 
 @app.on_message(filters.command("start"))
@@ -26,10 +26,10 @@ async def start(client, message):
             [
                 [
                      InlineKeyboardButton(
-                        text="Channel 🙋‍♀️", url="https://t.me/SDBOTs_Inifinity"
+                        text="Channel 🙋‍♀️", url="https://t.me/Z_Bots"
                     ),
                     InlineKeyboardButton(
-                        text="Dev 🔥", url="https://t.me/Itz_Sadew"
+                        text="Dev 🔥", url="https://github.com/madtoazenzio"
                     )
                 ]
             ]
